a10753da5171bf1089908ae37a0e8da678b8bae9
Adwasar <user@Users-MacBook-Pro.local>
2022-11-04 23:57:44 +0200

    fead: done task 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const pathToStyles = path.join(__dirname, 'styles');
+const pathToBundle = path.join(__dirname, 'project-dist');
+
+(async function bundleStyles() {
+  const filesAll = await fs.readdir(pathToStyles, {withFileTypes: true});
+  const stylesAll = [];
+
+  filesAll.forEach(async (file) => {
+    const currentPath = path.join(pathToStyles, file.name);
+
+    if (path.extname(currentPath) === '.css' ) {
+      console.log(file.name);
+      const fileContent = await fs.readFile(currentPath, 'utf8');
+      stylesAll.push(fileContent);
+    }
+
+    await fs.writeFile(path.join(pathToBundle, 'bundle.css'), stylesAll);
+  });
+})();
