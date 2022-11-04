1cb8f000831db7e9712b093df67df6d4ece675dc
Adwasar <user@Users-MacBook-Pro.local>
2022-11-04 19:08:38 +0200

    fead: done task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,12 @@
+const fs = require('fs/promises');
 
+(async function copyDir() {
+  const files = await fs.readdir('04-copy-directory/files');
+
+  await fs.mkdir('04-copy-directory/files-copy', {recursive: true});
+
+  files.forEach((file) => {
+    fs.copyFile(`04-copy-directory/files/${file}`, `04-copy-directory/files-copy/${file}`);
+  });
+
+})();
